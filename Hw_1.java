c81ea89e3395661bb7d23edce597d0d82311d76f
ReltTull <92668493+ReltTull@users.noreply.github.com>
2022-09-09 12:16:51 +0300

    ren

---
+++ Hw_1.java
@@ -0,0 +1,11 @@
+public class Hw_1 {
+//    private static int[] exception_1() {
+//        int a = 1000;
+//        return new int[a*a*a];
+//    }
+
+
+    public static void main(String[] args) {
+//        System.out.print(exception_1());
+    }
+}
