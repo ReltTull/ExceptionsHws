71663d2e4e3ef510a3e2a14955f8a306b337debc
ReltTull <92668493+ReltTull@users.noreply.github.com>
2022-09-09 19:09:16 +0300

    ex_4 added

--- Hw_1.java
+++ Hw_1.java
@@ -9,33 +9,34 @@ public class Hw_1 {
      * создание массива с негативным размером, превышение макисмального значение типа int.
      * @return
      */
-//    private static int[] exception_1() {
-//        int a = 1000;
-//        return new int[a*a*a*a];
-//    }
+    private static int[] exception_1() {
+        int a = 1000;
+        return new int[a*a*a*a];
+    }
 
     /**
      * индекс выходит из границ ArrayList.
      * @return
      */
-//    private static int exception_2() {
-//        ArrayList<Integer> arr = new ArrayList<>();
-//        return arr.get(2);
-//    }
+    private static int exception_2() {
+        ArrayList<Integer> arr = new ArrayList<>();
+        return arr.get(2);
+    }
 
     /**
      * Недопустимый аргумент.
      * @return
      */
-//    private static int exception_3(){
-//        ArrayList<Integer> input = new ArrayList<>();
-//        Random randomGenerator = new Random();
-//        return randomGenerator.nextInt(input.size());
-//    }
+    private static int exception_3(){
+        ArrayList<Integer> input = new ArrayList<>();
+        Random randomGenerator = new Random();
+        return randomGenerator.nextInt(input.size());
+    }
+
 
     private static int[] ex_3(int[] arr1, int[] arr2) {
         if (arr1.length != arr2.length) {
-            throw new RuntimeException("Arrays lengths is not equals");
+            System.out.println("Arrays lengths is not equals");
         }
         int[] res = new int[arr1.length];
         for (int i = 0; i < arr1.length; i++) {
@@ -44,12 +45,32 @@ public class Hw_1 {
         return res;
     }
 
+    private static double[] ex_4(int[] arr1, int[] arr2) {
+        if (arr1.length != arr2.length) {
+            throw new RuntimeException("Arrays lengths is not equals");
+        }
+        double[] res = new double[arr1.length];
+        for (int i = 0; i < arr1.length; i++) {
+            res[i] = (double) arr1[i] / (double) arr2[i];
+        }
+        return res;
+
+    }
+
 
     public static void main(String[] args) {
 
-        System.out.print(ex_3(new int[] {12, 3, 1, 4}, new int[] {1, 0, 20, 2}));
 //        System.out.print(exception_1());
 //        System.out.print(exception_2());
 //        System.out.print(exception_3());
+
+        int[] arr1 = {5, 4, 2, 10};
+        int[] arr2 = {2, 1, 2, 9};
+
+//        int[] res = ex_3(arr1, arr2);
+        double[] res = ex_4(arr1, arr2);
+        for (int i = 0; i < res.length; i++) {
+            System.out.print(res[i] + " ");
+        }
     }
 }
