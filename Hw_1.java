f44040f9acac69da4568dff5cd917910830e1e49
ReltTull <92668493+ReltTull@users.noreply.github.com>
2022-09-09 14:13:11 +0300

    exception_3 added

--- Hw_1.java
+++ Hw_1.java
@@ -1,9 +1,12 @@
+import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
 
 public class Hw_1 {
 
     /**
-     * создание массива с негативным размером, превышение макисмального значение типа int
+     * создание массива с негативным размером, превышение макисмального значение типа int.
      * @return
      */
 //    private static int[] exception_1() {
@@ -15,13 +18,25 @@ public class Hw_1 {
      * индекс выходит из границ ArrayList.
      * @return
      */
-    private static int exception_2() {
-        ArrayList<Integer> arr = new ArrayList<>();
-        return arr.get(2);
+//    private static int exception_2() {
+//        ArrayList<Integer> arr = new ArrayList<>();
+//        return arr.get(2);
+//    }
+
+    /**
+     * Недопустимый аргумент.
+     * @return
+     */
+    public static int exception_3(){
+        ArrayList<Integer> input = new ArrayList<>();
+        Random randomGenerator = new Random();
+        return randomGenerator.nextInt(input.size());
     }
+
 
     public static void main(String[] args) {
 //        System.out.print(exception_1());
-        System.out.print(exception_2());
+//        System.out.print(exception_2());
+        System.out.print(exception_3());
     }
 }
