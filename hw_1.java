a1b8407807077d1058b001d123279898e5e67930
ReltTull <92668493+ReltTull@users.noreply.github.com>
2022-09-09 12:15:08 +0300

    exception_1 added

---
+++ hw_1.java
@@ -0,0 +1,10 @@
+public class hw_1 {
+    private static int[] exception_1() {
+        int a = 1000;
+        return new int[a*a*a];
+    }
+
+    public static void main(String[] args) {
+        System.out.print(exception_1());
+    }
+}
