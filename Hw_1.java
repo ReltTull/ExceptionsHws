dbf4edbf1278273d56f3ce89a95ff6b7c74f52c9
ReltTull <92668493+ReltTull@users.noreply.github.com>
2022-09-09 14:19:59 +0300

    ex_3 added

--- Hw_1.java
+++ Hw_1.java
@@ -27,16 +27,29 @@ public class Hw_1 {
      * Недопустимый аргумент.
      * @return
      */
-    public static int exception_3(){
-        ArrayList<Integer> input = new ArrayList<>();
-        Random randomGenerator = new Random();
-        return randomGenerator.nextInt(input.size());
+//    private static int exception_3(){
+//        ArrayList<Integer> input = new ArrayList<>();
+//        Random randomGenerator = new Random();
+//        return randomGenerator.nextInt(input.size());
+//    }
+
+    private static int[] ex_3(int[] arr1, int[] arr2) {
+        if (arr1.length != arr2.length) {
+            throw new RuntimeException("Arrays lengths is not equals");
+        }
+        int[] res = new int[arr1.length];
+        for (int i = 0; i < arr1.length; i++) {
+            res[i] = arr1[i] = arr2[i];
+        }
+        return res;
     }
 
 
     public static void main(String[] args) {
+
+        System.out.print(ex_3(new int[] {12, 3, 1, 4}, new int[] {1, 0, 20, 2}));
 //        System.out.print(exception_1());
 //        System.out.print(exception_2());
-        System.out.print(exception_3());
+//        System.out.print(exception_3());
     }
 }
