ae65c3f64e165f624e8b63491a23575b4cc4b111
ReltTull <92668493+ReltTull@users.noreply.github.com>
2022-09-09 12:39:33 +0300

    Exceprion_2 added, comments

--- Hw_1.java
+++ Hw_1.java
@@ -1,11 +1,27 @@
+import java.util.ArrayList;
+
 public class Hw_1 {
+
+    /**
+     * создание массива с негативным размером, превышение макисмального значение типа int
+     * @return
+     */
 //    private static int[] exception_1() {
 //        int a = 1000;
-//        return new int[a*a*a];
+//        return new int[a*a*a*a];
 //    }
 
+    /**
+     * индекс выходит из границ ArrayList.
+     * @return
+     */
+    private static int exception_2() {
+        ArrayList<Integer> arr = new ArrayList<>();
+        return arr.get(2);
+    }
 
     public static void main(String[] args) {
 //        System.out.print(exception_1());
+        System.out.print(exception_2());
     }
 }
